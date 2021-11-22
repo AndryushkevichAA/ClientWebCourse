39a411166dd077e37af39ae068070899fa303254
AndryushkevichAA <aligato123123@gmail.com>
2021-11-22 19:36:41 +0700

    Объявление countries одним литералом, получение maxCitiesQuantity через reduce,getCountryWithMaxCitiesQuantity выдает массив объектов

--- Objects/js/script.js
+++ Objects/js/script.js
@@ -1,120 +1,119 @@
 (function () {
-    var russia = {
-        name: "Россия",
-        cities: [
-            {
-                name: "Москва",
-                population: 12655050
-            },
-            {
-                name: "Санкт-Петербург",
-                population: 5384342
-            },
-            {
-                name: "Новосибирск",
-                population: 1620162
-            },
-            {
-                name: "Екатеринбург",
-                population: 1495066
-            },
-            {
-                name: "Казань",
-                population: 1257341
-            },
-            {
-                name: "Нижний Новгород",
-                population: 1263650
-            }
-        ]
-    };
+    var countries = [
+        {
+            name: "Россия",
+            cities: [
+                {
+                    name: "Москва",
+                    population: 12655050
+                },
+                {
+                    name: "Санкт-Петербург",
+                    population: 5384342
+                },
+                {
+                    name: "Новосибирск",
+                    population: 1620162
+                },
+                {
+                    name: "Екатеринбург",
+                    population: 1495066
+                },
+                {
+                    name: "Казань",
+                    population: 1257341
+                },
+                {
+                    name: "Нижний Новгород",
+                    population: 1263650
+                }
+            ]
+        },
+        {
+            name: "Китай",
+            cities: [
+                {
+                    name: "Шанхай",
+                    population: 22315426
+                },
+                {
+                    name: "Пекин",
+                    population: 18827000
+                },
+                {
+                    name: "Гуанчжоу",
+                    population: 11070654
+                },
+                {
+                    name: "Гонконг",
+                    population: 7055071
+                }
+            ]
+        },
+        {
+            name: "США",
+            cities: [
+                {
+                    name: "Нью-Йорк",
+                    population: 8491079
+                },
+                {
+                    name: "Лос-Анджелес",
+                    population: 3792621
+                },
+                {
+                    name: "Чикаго",
+                    population: 2695598
+                },
+                {
+                    name: "Вашингтон",
+                    population: 601723
+                },
+                {
+                    name: "Лас-Вегас",
+                    population: 623747
+                }
+            ]
+        },
+        {
+            name: "Бразилия",
+            cities: [
+                {
+                    name: "Рио-де-Жанейро",
+                    population: 6023699
+                },
+                {
+                    name: "Сан-Паулу",
+                    population: 10021295
+                },
+                {
+                    name: "Сальвадор",
+                    population: 2711840
+                }
+            ]
+        }
+    ]
 
-    var china = {
-        name: "Китай",
-        cities: [
-            {
-                name: "Шанхай",
-                population: 22315426
-            },
-            {
-                name: "Пекин",
-                population: 18827000
-            },
-            {
-                name: "Гуанчжоу",
-                population: 11070654
-            },
-            {
-                name: "Гонконг",
-                population: 7055071
-            }
-        ]
-    };
-
-    var usa = {
-        name: "США",
-        cities: [
-            {
-                name: "Нью-Йорк",
-                population: 8491079
-            },
-            {
-                name: "Лос-Анджелес",
-                population: 3792621
-            },
-            {
-                name: "Чикаго",
-                population: 2695598
-            },
-            {
-                name: "Вашингтон",
-                population: 601723
-            },
-            {
-                name: "Лас-Вегас",
-                population: 623747
-            }
-        ]
-    };
-
-    var brazil = {
-        name: "Бразилия",
-        cities: [
-            {
-                name: "Рио-де-Жанейро",
-                population: 6023699
-            },
-            {
-                name: "Сан-Паулу",
-                population: 10021295
-            },
-            {
-                name: "Сальвадор",
-                population: 2711840
-            }
-        ]
-    };
-
-    var countries = [russia, china, usa, brazil];
     console.log(countries);
 
-    function getCountryWithMaxCitiesQuantity(countries) {
-        var maxCitiesQuantity = 0;
-
-        countries.forEach(function (country) {
+    function getCountriesWithMaxCitiesQuantity(countries) {
+        var maxCitiesQuantity = countries.reduce(function (maxCitiesQuantity, country) {
             if (country.cities.length > maxCitiesQuantity) {
                 maxCitiesQuantity = country.cities.length;
             }
-        });
+
+            return maxCitiesQuantity;
+        }, 0);
 
         return countries.filter(function (country) {
             return country.cities.length === maxCitiesQuantity;
         }).map(function (country) {
-            return country.name;
+            return country;
         });
     }
 
-    console.log("Страна с наибольшим количеством городов: " + getCountryWithMaxCitiesQuantity(countries));
+    console.log("Страны с наибольшим количеством городов: ");
+    console.log(getCountriesWithMaxCitiesQuantity(countries));
 
     function getCountriesWithPopulationSum(countries) {
         var countriesWithPopulationSum = {};
